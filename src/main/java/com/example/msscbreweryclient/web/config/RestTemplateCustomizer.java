973503394803ec32467782ac211ce4cfe8272b88
mastermind <imindloop@gmail.com>
2023-01-13 08:55:11 -0700

    Customizing the Rest Template Http Client to improve performance using Apache Async.

--- src/main/java/com/example/msscbreweryclient/web/config/RestTemplateCustomizer.java
+++ src/main/java/com/example/msscbreweryclient/web/config/RestTemplateCustomizer.java
@@ -2,11 +2,12 @@ package com.example.msscbreweryclient.web.config;
 
 import org.springframework.web.client.RestTemplate;
 
-public abstract class RestTemplateCustomizer {
+@FunctionalInterface
+public interface RestTemplateCustomizer {
 
     /**
-     * Callback to customize a {@Link RestTemplate} instance.
-     * @param restTemplate the template to customize
+     * A method to customize a {@Link RestTemplate} instance
+     * @param restTemplate the rest template instance to customize.
      */
-    abstract void customize(RestTemplate restTemplate);
+    void customize(RestTemplate restTemplate);
 }
